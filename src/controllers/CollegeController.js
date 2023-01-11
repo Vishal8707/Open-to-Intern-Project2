96563d1cdac28fea565c0dddf217aa1bb9cfcaa9
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-01-12 00:44:50 +0530

    done my project-2

--- src/controllers/CollegeController.js
+++ src/controllers/CollegeController.js
@@ -1,12 +1,13 @@
 const collegeModel = require("../models/CollegeModel");
 
 let nameRegex = /^[a-zA-Z ]+$/;
+let fullNameRegex = /^[a-z A-Z,]{1,50}$/;
 let logoLinkRrgex =  /^https?:\/\/(.+\/)+.+(\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif))$/i
 
 const createCollegeData = async (req, res) => {
     try {
         let data = req.body
-        let { name, fullName, logoLink } = data
+        let { name, fullName, logoLink,isDeleted } = data
         if (Object.keys(data).length === 0) {
             return res.status(400).send({ status: false, msg: "please provide all details" })
         }
@@ -27,14 +28,17 @@ const createCollegeData = async (req, res) => {
         if(!fullName || fullName == ""){
             return res.status(400).send({status:false, msg :"please provide the fullName"})
         }
-        if (!nameRegex.test(fullName)) {
+        if (!fullNameRegex.test(fullName)) {
             return res.status(400).send({ status: false, msg: "please enter valid fullName" })
         }
         if (!logoLink || logoLink == "") {
-            return res.status(400).send({ status: false, msg: "please provide the name" })
+            return res.status(400).send({ status: false, msg: "please provide the logoLink" })
         }
         if (!logoLinkRrgex.test(logoLink)) {
-            return res.status(400).send({ status: false, msg: "please enter valid fullName" })
+            return res.status(400).send({ status: false, msg: "please enter valid logoLink" })
+        }
+        if(isDeleted == true){
+            return res.status(400).send({ status: false, msg: "please check your isDeleted key status " })
         }
 
         let saveData = await collegeModel.create(data)
--- src/controllers/InernController.js
+++ src/controllers/InernController.js
@@ -3,9 +3,10 @@ const collegeModel = require("../models/CollegeModel");
 
 let nameRegex = /^[a-zA-Z ]+$/;
 
-let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z]+(?:\.[a-zA-Z]+)*$/;
 
-let mobileRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+let mobileRegex =
+  /^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/;
 
 const createIntern = async function (req, res) {
   try {
@@ -71,50 +72,50 @@ const createIntern = async function (req, res) {
     }
     data.collegeId = findCollege._id;
     let createIntern = await InternModel.create(data);
-    res
-      .status(201)
-      .send({
-        status: false,
-        msg: "your data is successfully created",
-        data: createIntern,
-      });
+    res.status(201).send({
+      status: false,
+      msg: "your data is successfully created",
+      data: createIntern,
+    });
   } catch (error) {
     res.status(500).send({ msg: error.msg });
   }
 };
 const getCollege = async function (req, res) {
   try {
-    let queryParam = req.query;
+    let data = req.query.collegeName;
 
-    if (Object.keys(queryParam).length == 0)
+    if (!data || data == "") {
+      return res.status(400).send({
+        status: false,
+        msg: "please enter the college Name",
+      });
+    }
+    if (!nameRegex.test(data)) {
       return res
         .status(400)
-        .send({
-          status: false,
-          msg: "Give the data first for fetching document!!!",
-        });
+        .send({ status: false, msg: "please enter valid collegeName" });
+    }
 
-    let findCollege = await collegeModel.findOne({
-      name: queryParam.collegeName,
-    });
-    let { name, fullName, logoLink, _id } = findCollege;
+    let findCollege = await collegeModel.findOne({name: data,isDeleted: false,});
+
+    if (!findCollege) {
+      return res.status(404).send({status: false,msg: "sorry no collage data found with this collegeName" });
+    }
+    let{name,fullName,logoLink,_id} = findCollege
 
     let internCollege = await InternModel.find({
       collegeId: _id,
       isDeleted: false,
     }).select({ name: 1, email: 1, mobile: 1 });
-
+  
     if (internCollege.length == 0)
-      return res
-        .status(400)
-        .send({
-          status: false,
-          msg: "Don't have any document with this query data in database.",
-        });
-
-    res
-      .status(200)
-      .send({
+      return res.status(400).send({
+        status: false,
+        msg: "Don't have any document with this query data in database.",
+      });
+    if (internCollege)
+      return res.status(200).send({
         status: true,
         data: { name, fullName, logoLink, interns: internCollege },
       });
